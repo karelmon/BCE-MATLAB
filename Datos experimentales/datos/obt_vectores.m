597a6c2046eb4d4e664e5fc323389fe870d2a1c1
cristhianapaza <crissapazaf@gmail.com>
2024-03-20 16:20:19 -0400

    vect_sod

--- Datos experimentales/datos/obt_vectores.m
+++ Datos experimentales/datos/obt_vectores.m
@@ -1,10 +1,10 @@
 clc; clear; 
-filename = 'um_0.03_50.xlsm';
+filename = 'um_0.56_50.xlsm';
 sheet = 1;
-xlRange = 'B1:B697';
-ylRange = 'E1:E697';
-nRange = 'G1:G697';
-zlRange = 'I1:I697';
+xlRange = 'B1:B1143';
+ylRange = 'E1:E1143';
+nRange = 'G1:G1143';
+zlRange = 'I1:I1143';
 out_sod = xlsread(filename,sheet,xlRange) ;
 in_sod = xlsread(filename,sheet,ylRange);
 num_eventos = xlsread(filename,sheet,nRange);
